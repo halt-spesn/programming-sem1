aa791ff5ffbce4c5fde58ef81dbb64515c1946a4
Безвершук Ростислав <ki261_brv@student.ztu.edu.ua>
2026-09-17 17:15:44 +0300

    feat(lab1): complete compilation pipeline research

---
+++ lab01/pipeline.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define ARCH_ID 123
+
+int main(void) {
+    // Лабораторна робота №1: Конвеєр компіляції
+    printf("Computer Engineering Specialty Code: %d\n", ARCH_ID);
+    return 0;
+}
